c332cd106fa63e9bc26e7afe70de2b3739e1376c
xulibin <xulibin@163.com>
2021-11-09 10:12:20 +0800

    基础的路由配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,17 +1,16 @@
 <?php
 namespace app\index\controller;
 
-
+use think\Controller;
 //导入命名空间的类库
 
-class Index
+class Index extends Controller
 {
-
-        public function index(){
-          return 'index';
-        }
-
-        public function hello($name = 'world',$city=''){
-          return 'Hello,' . $name . '! You come from '. $city .'.' ;
-     }
+    public function hello($name = 'World'){
+       // Request::instance()获取post、get、参数、表单上传的文件
+//        $request = Request::instance();
+        //获取当前URL地址  不含域名
+        echo 'url:' . $this->request->url() . '<br/>';
+        return 'Hello,' .$name . '!';
+    }
 }
--- application/route.php
+++ application/route.php
@@ -18,5 +18,16 @@ return [
         ':name' => ['index/hello', ['method' => 'post']],
     ],
     //添加路由规则路由到index控制器的hello方法
-    'hello/:name' => 'index/index/hello',
+    //    'hello/:name' => 'index/index/hello'
+
+    //修改路由规则，上面的配置必须传入参数才可以正确访问，使用[]表示该变量为可选,http://tp5/public/hello
+    //'hello/[:name]' => 'index/hello'
+
+    //完整匹配,表示当前路由规则需要完整匹配:http://tp5/public/hello或着http://tp5/public/hello/thinkphp
+    'hello/[:name]$' => 'index/hello'
+
+    //闭包定义
+    //'hello/:name' => function($name){
+    //     return 'Hello,' . $name . '!';
+    //}
 ];
