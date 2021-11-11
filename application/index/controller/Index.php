d5534dc5e24a81c5cedd2826a423ae2917e9613e
xulibin <xulibin@163.com>
2021-11-11 16:03:54 +0800

    获取请求变量

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,27 +1,24 @@
 <?php
 namespace app\index\controller;
 
-//动态绑定属性
-//use app\index\model\User;
-//use think\Controller;
-//use think\Request;
-//use think\Session;
-//导入命名空间的类库
+use think\Request;
+class Index{
 
-class Index
-{
-//   public function _initialize(){
-//       $user = User::get(Session::get('user_id'));
-//       Request::instance() -> bind('user',$user);
-//   }
 
-//使用助手函数--->没有继承think\Controller也不想给操作方法添加额外的Request对象参数
+public function hello(Request $request){
+    //http://localhost:8888/tp5/public/index/index/hello.html?test=add&name=thinkphp
+    echo '请求参数:';
+//    dump($request -> param());
+//    echo 'name:' .$request->param('name');
 
-public function hello($name ='World'){
+    //使用input函数来简化请求，会自动判断当前的请求
+//    dump(input());
+//    echo 'name:' .input('name');
 
-    //获取当前url地址  不含域名
-    echo 'url:' .request() -> url() . '<br />';
-    return 'Hello,' .$name .'!';
-}
+    //param方法获取的参数会自动判断当前的请求：路由变量>当前请求变量>$GET变量
+
+    echo 'name:' .$request->param('name','World','strtolower');
+    echo '<br/>test:' .$request->param('test','thinkphp','strtoupper');
+  }
 
 }
